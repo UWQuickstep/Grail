7ddd4602e0cbe2bc4273cd4cddc8c5f2dc5bf60d
Adalbert Gerald Soosai Raj <gerald@cs.wisc.edu>
2016-01-20 16:30:59 -0600

    Added separate datagens for different algorithms.

---
+++ datagen/wcc/create_and_load_edge.sql
@@ -0,0 +1,7 @@
+create table edge(src int, dest int);
+insert into edge values(1,2);
+insert into edge values(2,1);
+insert into edge values(2,3);
+insert into edge values(3,4);
+insert into edge values(4,3);
+select * from edge;
