6f01cfb1fc97e99cea33102c3ded5d0b1ec58e95
Jignesh Patel <jmp_007@hotmail.com>
2016-03-03 14:27:12 -0600

    Pull back lost changes to the Main.py.

--- Main.py
+++ Main.py
@@ -1,22 +1,39 @@
 from Grail import Grail
 
+#!/usr/bin/python
 
 import sys, getopt
 
 def main(argv):
-    inputfile = 'config.grail'
-    outputfile = 'output'
+  inputfile = ''
+  outputfile = ''
+  print inputfile, outputfile
 
-    grail=Grail(inputfile)
-    grail.run()
-    f=open(outputfile,'w')
-    outputString=""
-    for block in grail.getBlocks():
-        ret=block.getBlockSql()
-        outputString+=ret+"\n"
+  try:
+    opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+  except getopt.GetoptError:
+    print 'test.py -i <inputfile> -o <outputfile>'
+    sys.exit(2)
 
-    f.write(outputString)
-    print "Successflly completed Grail execution. Please check the ouput file for results.\n"
+  for opt, arg in opts:
+    if opt == '-h':
+      print 'test.py -i <inputfile> -o <outputfile>'
+      sys.exit()
+    elif opt in ("-i", "--ifile"):
+      inputfile = arg
+    elif opt in ("-o", "--ofile"):
+      outputfile = arg
+
+  grail=Grail(inputfile)
+  grail.run()
+
+  f=open(outputfile,'w')
+  outputString=""
+  for block in grail.getBlocks():
+    ret=block.getBlockSql()
+    outputString+=ret+"\n"
+  f.write(outputString)
+  print "Successflly completed Grail execution. Please check the ouput file for results.\n"
 
 if __name__ == "__main__":
     main(sys.argv[1:])
