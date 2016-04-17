af94042d805ed3fdce839a90c3bde4ec325c3f80
Subasree Venkatsubhramaniyen <subasreev@cs.wisc.edu>
2016-04-17 13:04:19 -0500

    test6 sssp

---
+++ tests/sssp/create_and_load_edge_test6.sql
@@ -0,0 +1,12 @@
+drop table IF EXISTS edge;
+create table edge(src int, dest int, weight int);
+insert into edge values(1,2,2);
+insert into edge values(1,3,9);
+insert into edge values(1,4,4);
+insert into edge values(1,5,6);
+insert into edge values(2,3,1);
+insert into edge values(3,4,2);
+insert into edge values(4,3,3);
+insert into edge values(4,5,1);
+insert into edge values(5,1,2);
+select * from edge;
