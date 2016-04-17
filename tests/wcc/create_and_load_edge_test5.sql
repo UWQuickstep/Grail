1971bbc5e8a3f43828cfbe425003f0a7afc48ede
Subasree Venkatsubhramaniyen <subasreev@cs.wisc.edu>
2016-04-16 20:50:10 -0500

    added test5 for wcc

---
+++ tests/wcc/create_and_load_edge_test5.sql
@@ -0,0 +1,10 @@
+drop table IF EXISTS edge;
+create table edge(src int, dest int);
+insert into edge values(1,2);
+insert into edge values(2,3);
+insert into edge values(3,1);
+insert into edge values(2,4);
+insert into edge values(4,5);
+insert into edge values(5,6);
+insert into edge values(6,4);
+select * from edge;
---
+++ tests/wcc/create_and_load_vertex_test5.sql
@@ -0,0 +1,11 @@
+drop table IF EXISTS vertex;
+create table vertex(id int);
+insert into vertex values(1);
+insert into vertex values(2);
+insert into vertex values(3);
+insert into vertex values(4);
+insert into vertex values(5);
+insert into vertex values(6);
+insert into vertex values(7);
+select * from vertex;
+
